2821d661443e39e52829e9ab9e6486302ee7acc4
21020764-VuPhuongHong <21020764@vnu.edu.vn>
2023-09-29 00:43:44 +0700

    update

--- src/test/java/TaxAssessment01Test.java
+++ src/test/java/TaxAssessment01Test.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+
 import static junit.framework.TestCase.assertEquals;
 
 /**
@@ -8,286 +9,286 @@ import static junit.framework.TestCase.assertEquals;
 public class TaxAssessment01Test {
     @Test
     public void testFormatTax1() {
-        assertEquals(TaxAssessment.formatTax("A", -25000000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("A", -25000000));
     }
 
     @Test
     public void testFormatTax2() {
-        assertEquals(TaxAssessment.formatTax("A", -1), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("A", -1));
     }
 
     @Test
     public void testFormatTax3() {
-        assertEquals(TaxAssessment.formatTax("A", 0), "0");
+        assertEquals("0", TaxAssessment.formatTax("A", 0));
     }
 
     @Test
     public void testFormatTax4() {
-        assertEquals(TaxAssessment.formatTax("A", 1000000), "50,000");
+        assertEquals("50,000", TaxAssessment.formatTax("A", 1000000));
     }
 
     @Test
     public void testFormatTax5() {
-        assertEquals(TaxAssessment.formatTax("A", 1999999), "100,000");
+        assertEquals("100,000", TaxAssessment.formatTax("A", 1999999));
     }
 
     @Test
     public void testFormatTax6() {
-        assertEquals(TaxAssessment.formatTax("A", 2000000), "100,000");
+        assertEquals("100,000", TaxAssessment.formatTax("A", 2000000));
     }
 
     @Test
     public void testFormatTax7() {
-        assertEquals(TaxAssessment.formatTax("A", 3500000), "175,000");
+        assertEquals("175,000", TaxAssessment.formatTax("A", 3500000));
     }
 
     @Test
     public void testFormatTax8() {
-        assertEquals(TaxAssessment.formatTax("A", 5000000), "250,000");
+        assertEquals("250,000", TaxAssessment.formatTax("A", 5000000));
     }
 
     @Test
     public void testFormatTax9() {
-        assertEquals(TaxAssessment.formatTax("A", 5000001), "250,000");
+        assertEquals("250,000", TaxAssessment.formatTax("A", 5000001));
     }
 
     @Test
     public void testFormatTax10() {
-        assertEquals(TaxAssessment.formatTax("A", 8000000), "550,000");
+        assertEquals("550,000", TaxAssessment.formatTax("A", 8000000));
     }
 
     @Test
     public void testFormatTax11() {
-        assertEquals(TaxAssessment.formatTax("A", 10000000), "750,000");
+        assertEquals("750,000", TaxAssessment.formatTax("A", 10000000));
     }
 
     @Test
     public void testFormatTax12() {
-        assertEquals(TaxAssessment.formatTax("A", 10000001), "750,000");
+        assertEquals("750,000", TaxAssessment.formatTax("A", 10000001));
     }
 
     @Test
     public void testFormatTax13() {
-        assertEquals(TaxAssessment.formatTax("A", 13800000), "1,320,000");
+        assertEquals("1,320,000", TaxAssessment.formatTax("A", 13800000));
     }
 
     @Test
     public void testFormatTax14() {
-        assertEquals(TaxAssessment.formatTax("A", 18000000), "1,950,000");
+        assertEquals("1,950,000", TaxAssessment.formatTax("A", 18000000));
     }
 
     @Test
     public void testFormatTax15() {
-        assertEquals(TaxAssessment.formatTax("A", 18000001), "1,950,000");
+        assertEquals("1,950,000", TaxAssessment.formatTax("A", 18000001));
     }
 
     @Test
     public void testFormatTax16() {
-        assertEquals(TaxAssessment.formatTax("A", 26050000), "3,560,000");
+        assertEquals("3,560,000", TaxAssessment.formatTax("A", 26050000));
     }
 
     @Test
     public void testFormatTax17() {
-        assertEquals(TaxAssessment.formatTax("A", 32000000), "4,750,000");
+        assertEquals("4,750,000", TaxAssessment.formatTax("A", 32000000));
     }
 
     @Test
     public void testFormatTax18() {
-        assertEquals(TaxAssessment.formatTax("A", 32000001), "4,750,000");
+        assertEquals("4,750,000", TaxAssessment.formatTax("A", 32000001));
     }
 
     @Test
     public void testFormatTax19() {
-        assertEquals(TaxAssessment.formatTax("A", 47200000), "8,550,000");
+        assertEquals("8,550,000", TaxAssessment.formatTax("A", 47200000));
     }
 
     @Test
     public void testFormatTax20() {
-        assertEquals(TaxAssessment.formatTax("A", 52000000), "9,750,000");
+        assertEquals("9,750,000", TaxAssessment.formatTax("A", 52000000));
     }
 
     @Test
     public void testFormatTax21() {
-        assertEquals(TaxAssessment.formatTax("A", 52000001), "9,750,000");
+        assertEquals("9,750,000", TaxAssessment.formatTax("A", 52000001));
     }
 
     @Test
     public void testFormatTax22() {
-        assertEquals(TaxAssessment.formatTax("A", 73900000), "16,320,000");
+        assertEquals("16,320,000", TaxAssessment.formatTax("A", 73900000));
     }
 
     @Test
     public void testFormatTax23() {
-        assertEquals(TaxAssessment.formatTax("A", 80000000), "18,150,000");
+        assertEquals("18,150,000", TaxAssessment.formatTax("A", 80000000));
     }
 
     @Test
     public void testFormatTax24() {
-        assertEquals(TaxAssessment.formatTax("A", 80000001), "18,150,000");
+        assertEquals("18,150,000", TaxAssessment.formatTax("A", 80000001));
     }
 
     @Test
     public void testFormatTax25() {
-        assertEquals(TaxAssessment.formatTax("A", 780620000), "263,367,000");
+        assertEquals("263,367,000", TaxAssessment.formatTax("A", 780620000));
     }
 
     @Test
     public void testFormatTax26() {
-        assertEquals(TaxAssessment.formatTax("B", -500000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("B", -500000));
     }
 
     @Test
     public void testFormatTax27() {
-        assertEquals(TaxAssessment.formatTax("B", -1), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("B", -1));
     }
 
     @Test
     public void testFormatTax28() {
-        assertEquals(TaxAssessment.formatTax("B", 0), "0");
+        assertEquals("0", TaxAssessment.formatTax("B", 0));
     }
 
     @Test
     public void testFormatTax29() {
-        assertEquals(TaxAssessment.formatTax("B", 1500000), "0");
+        assertEquals("0", TaxAssessment.formatTax("B", 1500000));
     }
 
     @Test
     public void testFormatTax30() {
-        assertEquals(TaxAssessment.formatTax("B", 1999999), "0");
+        assertEquals("0", TaxAssessment.formatTax("B", 1999999));
     }
 
     @Test
     public void testFormatTax31() {
-        assertEquals(TaxAssessment.formatTax("B", 2000000), "200,000");
+        assertEquals("200,000", TaxAssessment.formatTax("B", 2000000));
     }
 
     @Test
     public void testFormatTax32() {
-        assertEquals(TaxAssessment.formatTax("B", 3500000), "350,000");
+        assertEquals("350,000", TaxAssessment.formatTax("B", 3500000));
     }
 
     @Test
     public void testFormatTax33() {
-        assertEquals(TaxAssessment.formatTax("B", 8000000), "800,000");
+        assertEquals("800,000", TaxAssessment.formatTax("B", 8000000));
     }
 
     @Test
     public void testFormatTax34() {
-        assertEquals(TaxAssessment.formatTax("B", 14000000), "1,400,000");
+        assertEquals("1,400,000", TaxAssessment.formatTax("B", 14000000));
     }
 
     @Test
     public void testFormatTax35() {
-        assertEquals(TaxAssessment.formatTax("B", 29000000), "2,900,000");
+        assertEquals("2,900,000", TaxAssessment.formatTax("B", 29000000));
     }
 
     @Test
     public void testFormatTax36() {
-        assertEquals(TaxAssessment.formatTax("B", 37500000), "3,750,000");
+        assertEquals("3,750,000", TaxAssessment.formatTax("B", 37500000));
     }
 
     @Test
     public void testFormatTax37() {
-        assertEquals(TaxAssessment.formatTax("B", 65000000), "6,500,000");
+        assertEquals("6,500,000", TaxAssessment.formatTax("B", 65000000));
     }
 
     @Test
     public void testFormatTax38() {
-        assertEquals(TaxAssessment.formatTax("B", 120000000), "12,000,000");
+        assertEquals("12,000,000", TaxAssessment.formatTax("B", 120000000));
     }
 
     @Test
     public void testFormatTax39() {
-        assertEquals(TaxAssessment.formatTax("C", -20000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("C", -20000));
     }
 
     @Test
     public void testFormatTax40() {
-        assertEquals(TaxAssessment.formatTax("C", -1), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("C", -1));
     }
 
     @Test
     public void testFormatTax41() {
-        assertEquals(TaxAssessment.formatTax("C", 0), "0");
+        assertEquals("0", TaxAssessment.formatTax("C", 0));
     }
 
     @Test
     public void testFormatTax42() {
-        assertEquals(TaxAssessment.formatTax("C", 1000000), "200,000");
+        assertEquals("200,000", TaxAssessment.formatTax("C", 1000000));
     }
 
     @Test
     public void testFormatTax43() {
-        assertEquals(TaxAssessment.formatTax("C", 4000000), "800,000");
+        assertEquals("800,000", TaxAssessment.formatTax("C", 4000000));
     }
 
     @Test
     public void testFormatTax44() {
-        assertEquals(TaxAssessment.formatTax("C", 6800000), "1,360,000");
+        assertEquals("1,360,000", TaxAssessment.formatTax("C", 6800000));
     }
 
     @Test
     public void testFormatTax45() {
-        assertEquals(TaxAssessment.formatTax("C", 16000000), "3,200,000");
+        assertEquals("3,200,000", TaxAssessment.formatTax("C", 16000000));
     }
 
     @Test
     public void testFormatTax46() {
-        assertEquals(TaxAssessment.formatTax("C", 23500000), "4,700,000");
+        assertEquals("4,700,000", TaxAssessment.formatTax("C", 23500000));
     }
 
     @Test
     public void testFormatTax47() {
-        assertEquals(TaxAssessment.formatTax("C", 35000000), "7,000,000");
+        assertEquals("7,000,000", TaxAssessment.formatTax("C", 35000000));
     }
 
     @Test
     public void testFormatTax48() {
-        assertEquals(TaxAssessment.formatTax("C", 70000000), "14,000,000");
+        assertEquals("14,000,000", TaxAssessment.formatTax("C", 70000000));
     }
 
     @Test
     public void testFormatTax49() {
-        assertEquals(TaxAssessment.formatTax("C", 198500000), "39,700,000");
+        assertEquals("39,700,000", TaxAssessment.formatTax("C", 198500000));
     }
 
     @Test
     public void testFormatTax50() {
-        assertEquals(TaxAssessment.formatTax("1", 0), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("1", 0));
     }
 
     @Test
     public void testFormatTax51() {
-        assertEquals(TaxAssessment.formatTax("2", 3000000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("2", 3000000));
     }
 
     @Test
     public void testFormatTax52() {
-        assertEquals(TaxAssessment.formatTax("3", 6500000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("3", 6500000));
     }
 
     @Test
     public void testFormatTax53() {
-        assertEquals(TaxAssessment.formatTax("a", 15000000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("a", 15000000));
     }
 
     @Test
     public void testFormatTax54() {
-        assertEquals(TaxAssessment.formatTax("b", 20000000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("b", 20000000));
     }
 
     @Test
     public void testFormatTax55() {
-        assertEquals(TaxAssessment.formatTax("c", 39000000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("c", 39000000));
     }
 
     @Test
     public void testFormatTax56() {
-        assertEquals(TaxAssessment.formatTax("mn", 72000000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("mn", 72000000));
     }
 
     @Test
     public void testFormatTax57() {
-        assertEquals(TaxAssessment.formatTax("@", 900000000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("@", 900000000));
     }
 }
--- src/test/java/TaxAssessment02Test.java
+++ src/test/java/TaxAssessment02Test.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+
 import static junit.framework.TestCase.assertEquals;
 
 /**
@@ -8,182 +9,182 @@ import static junit.framework.TestCase.assertEquals;
 public class TaxAssessment02Test {
     @Test
     public void testFormatTax1() {
-        assertEquals(TaxAssessment.formatTax("A", -100000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("A", -100000));
     }
 
     @Test
     public void testFormatTax2() {
-        assertEquals(TaxAssessment.formatTax("A", 1250000), "62,500");
+        assertEquals("62,500", TaxAssessment.formatTax("A", 1250000));
     }
 
     @Test
     public void testFormatTax3() {
-        assertEquals(TaxAssessment.formatTax("A", 4700000), "235,000");
+        assertEquals("235,000", TaxAssessment.formatTax("A", 4700000));
     }
 
     @Test
     public void testFormatTax4() {
-        assertEquals(TaxAssessment.formatTax("A", 8100000), "560,000");
+        assertEquals("560,000", TaxAssessment.formatTax("A", 8100000));
     }
 
     @Test
     public void testFormatTax5() {
-        assertEquals(TaxAssessment.formatTax("A", 15400000), "1,560,000");
+        assertEquals("1,560,000", TaxAssessment.formatTax("A", 15400000));
     }
 
     @Test
     public void testFormatTax6() {
-        assertEquals(TaxAssessment.formatTax("A", 24000000), "3,150,000");
+        assertEquals("3,150,000", TaxAssessment.formatTax("A", 24000000));
     }
 
     @Test
     public void testFormatTax7() {
-        assertEquals(TaxAssessment.formatTax("A", 37500000), "6,125,000");
+        assertEquals("6,125,000", TaxAssessment.formatTax("A", 37500000));
     }
 
     @Test
     public void testFormatTax8() {
-        assertEquals(TaxAssessment.formatTax("A", 64000000), "13,350,000");
+        assertEquals("13,350,000", TaxAssessment.formatTax("A", 64000000));
     }
 
     @Test
     public void testFormatTax9() {
-        assertEquals(TaxAssessment.formatTax("A", 270000000), "84,650,000");
+        assertEquals("84,650,000", TaxAssessment.formatTax("A", 270000000));
     }
 
     @Test
     public void testFormatTax10() {
-        assertEquals(TaxAssessment.formatTax("B", -100000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("B", -100000));
     }
 
     @Test
     public void testFormatTax11() {
-        assertEquals(TaxAssessment.formatTax("B", 1250000), "0");
+        assertEquals("0", TaxAssessment.formatTax("B", 1250000));
     }
 
     @Test
     public void testFormatTax12() {
-        assertEquals(TaxAssessment.formatTax("B", 4700000), "470,000");
+        assertEquals("470,000", TaxAssessment.formatTax("B", 4700000));
     }
 
     @Test
     public void testFormatTax13() {
-        assertEquals(TaxAssessment.formatTax("B", 8100000), "810,000");
+        assertEquals("810,000", TaxAssessment.formatTax("B", 8100000));
     }
 
     @Test
     public void testFormatTax14() {
-        assertEquals(TaxAssessment.formatTax("B", 15400000), "1,540,000");
+        assertEquals("1,540,000", TaxAssessment.formatTax("B", 15400000));
     }
 
     @Test
     public void testFormatTax15() {
-        assertEquals(TaxAssessment.formatTax("B", 24000000), "2,400,000");
+        assertEquals("2,400,000", TaxAssessment.formatTax("B", 24000000));
     }
 
     @Test
     public void testFormatTax16() {
-        assertEquals(TaxAssessment.formatTax("B", 37500000), "3,750,000");
+        assertEquals("3,750,000", TaxAssessment.formatTax("B", 37500000));
     }
 
     @Test
     public void testFormatTax17() {
-        assertEquals(TaxAssessment.formatTax("B", 64000000), "6,400,000");
+        assertEquals("6,400,000", TaxAssessment.formatTax("B", 64000000));
     }
 
     @Test
     public void testFormatTax18() {
-        assertEquals(TaxAssessment.formatTax("B", 270000000), "27,000,000");
+        assertEquals("27,000,000", TaxAssessment.formatTax("B", 270000000));
     }
 
     @Test
     public void testFormatTax19() {
-        assertEquals(TaxAssessment.formatTax("C", -100000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("C", -100000));
     }
 
     @Test
     public void testFormatTax20() {
-        assertEquals(TaxAssessment.formatTax("C", 1250000), "250,000");
+        assertEquals("250,000", TaxAssessment.formatTax("C", 1250000));
     }
 
     @Test
     public void testFormatTax21() {
-        assertEquals(TaxAssessment.formatTax("C", 4700000), "940,000");
+        assertEquals("940,000", TaxAssessment.formatTax("C", 4700000));
     }
 
     @Test
     public void testFormatTax22() {
-        assertEquals(TaxAssessment.formatTax("C", 8100000), "1,620,000");
+        assertEquals("1,620,000", TaxAssessment.formatTax("C", 8100000));
     }
 
     @Test
     public void testFormatTax23() {
-        assertEquals(TaxAssessment.formatTax("C", 15400000), "3,080,000");
+        assertEquals("3,080,000", TaxAssessment.formatTax("C", 15400000));
     }
 
     @Test
     public void testFormatTax24() {
-        assertEquals(TaxAssessment.formatTax("C", 24000000), "4,800,000");
+        assertEquals("4,800,000", TaxAssessment.formatTax("C", 24000000));
     }
 
     @Test
     public void testFormatTax25() {
-        assertEquals(TaxAssessment.formatTax("C", 37500000), "7,500,000");
+        assertEquals("7,500,000", TaxAssessment.formatTax("C", 37500000));
     }
 
     @Test
     public void testFormatTax26() {
-        assertEquals(TaxAssessment.formatTax("C", 64000000), "12,800,000");
+        assertEquals("12,800,000", TaxAssessment.formatTax("C", 64000000));
     }
 
     @Test
     public void testFormatTax27() {
-        assertEquals(TaxAssessment.formatTax("C", 270000000), "54,000,000");
+        assertEquals("54,000,000", TaxAssessment.formatTax("C", 270000000));
     }
 
     @Test
     public void testFormatTax28() {
-        assertEquals(TaxAssessment.formatTax("8", -100000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("8", -100000));
     }
 
     @Test
     public void testFormatTax29() {
-        assertEquals(TaxAssessment.formatTax("10", 1250000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("10", 1250000));
     }
 
     @Test
     public void testFormatTax30() {
-        assertEquals(TaxAssessment.formatTax("1.5", 4700000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("1.5", 4700000));
     }
 
     @Test
     public void testFormatTax31() {
-        assertEquals(TaxAssessment.formatTax("doituongA", 8100000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("doituongA", 8100000));
     }
 
     @Test
     public void testFormatTax32() {
-        assertEquals(TaxAssessment.formatTax("#", 15400000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("#", 15400000));
     }
 
     @Test
     public void testFormatTax33() {
-        assertEquals(TaxAssessment.formatTax("abc", 24000000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("abc", 24000000));
     }
 
     @Test
     public void testFormatTax34() {
-        assertEquals(TaxAssessment.formatTax("/", 37500000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("/", 37500000));
     }
 
     @Test
     public void testFormatTax35() {
-        assertEquals(TaxAssessment.formatTax("ABC", 64000000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("ABC", 64000000));
     }
 
     @Test
     public void testFormatTax36() {
-        assertEquals(TaxAssessment.formatTax("Doi Tuong B", 270000000), "Invalid input");
+        assertEquals("Invalid input", TaxAssessment.formatTax("Doi Tuong B", 270000000));
     }
 }
 
